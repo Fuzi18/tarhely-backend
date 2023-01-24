90e8355bbf63f1b805e151b6cbad7eeffad98ab2
Fuzi18 <fuzesi.t2003@gmail.com>
2023-01-24 15:01:44 +0100

    commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,61 @@
 import {
+  Body,
   Controller,
+  Delete,
   Get,
+  Param,
+  Post,
+  Redirect,
   Render,
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import db from './db';
+import TarhelyDataDto from './tarhelydata.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @Render('index')
-  index() {
-    return { message: 'Welcome to the homepage' };
+  @Render('tarhelyek')
+  registerForm() {
+    return {};
+  }
+
+  @Post()
+  @Redirect()
+  async register(@Body() userdata: TarhelyDataDto) {
+    await db.execute('INSERT INTO tarhelycsomagok (nev, meret, ar) VALUES (?, ?, ?)', [
+      userdata.nev, userdata.meret, userdata.ar
+    ]);
+    return {
+      url: '/',
+    };
+  }
+
+  @Get('/api/tarhely')
+  async allTarhely() {
+    const [tarhely] = await db.execute(
+      'SELECT * FROM tarhelycsomagok'
+    );
+    return { tarhely: tarhely };
+  }
+
+  @Post('/api/tarhely')
+  async registerApi(@Body() userdata: TarhelyDataDto) {
+    await db.execute('INSERT INTO tarhelycsomagok (nev, meret, ar) VALUES (?, ?, ?)', [
+      userdata.nev, userdata.meret, userdata.ar
+    ]);
+  }
+
+
+  @Delete('/api/tarhely/:id')
+  async deleteTarhelyApi(@Param('id') id: number) {
+    await db.execute(
+      'DELETE FROM tarhelycsomagok WHERE id = ?',
+      [id],
+    );
   }
 }
+
+
